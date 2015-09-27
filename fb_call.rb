25f81d8f9368c180d49fb10edaf2bef4a17c73ec
mikhail <mikhaila@thoughworks.com>
2015-09-27 10:54:17 +0300

    Mikhail: Using curl instead of rest-client gem

--- fb_call.rb
+++ fb_call.rb
@@ -1,5 +1,4 @@
 require 'pry'
-require 'rest-client'
 
 urls = File.readlines(ARGV[0])
 
@@ -7,11 +6,11 @@ start_index = ARGV[1].to_i
 length = ARGV[2].to_i
 urls = urls[start_index..(start_index+length-1)]
 urls.each do |url|
-  graph_api_call_response = RestClient.get("http://graph.facebook.com/?id=#{url}")
+  graph_api_call_response = `curl -s http://graph.facebook.com/?id=#{url}`
   not_scraped = graph_api_call_response.include?("\"is_scraped\":false")
-  RestClient.post("http://graph.facebook.com/?ids=#{url}&scrape=true","") if not_scraped
   status = not_scraped ? "NOT_SCRAPED" : "SCRAPED"
   puts "#{status},#{url}"
+  `curl -s -X POST "http://graph.facebook.com/?ids=#{url}&scrape=true"` if not_scraped
   sleep(0.5)
 end
 
